96865b44b6429be6dffb49e134db84c6c37c0375
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-10-20 17:19:18 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+### October 20, 2022
+
+A simple calculator to do basic operators. Make it a scientific calculator for added complexity.
---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together
+ * @param  {Number} first_parameter The first number
+ * @param  {Number} second_parameter The second number
+ * @return {Number}      The total of the two numbers
+ */
+function addTwoNumbers(first_parameter, second_parameter) {
+
+}
